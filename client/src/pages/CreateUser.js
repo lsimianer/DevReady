c2115394de8bfcdf020f32eb9ddf898bbb61c619
lukas simianer <lsimianer@gmail.com>
2019-08-02 14:35:46 -0500

    routes fully functional

---
+++ client/src/pages/CreateUser.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function CreateUser() {
+  return (
+    <div>
+      <h1>ViewDevs page</h1>
+      <p>
+        this is where users will create an account
+      </p>
+    </div>
+  );
+}
+
+export default CreateUser;
