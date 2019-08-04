99ecb3024ceb70756573c56674b1327f2d408505
lukas simianer <lsimianer@gmail.com>
2019-08-04 11:43:25 -0500

    test selct put into cards, package json added bootstrap and react strp

--- client/src/pages/TestSelector.js
+++ client/src/pages/TestSelector.js
@@ -1,10 +1,9 @@
-import React, { Component } from "react";
-import DeleteBtn from "../components/DeleteBtn";
 import Jumbotron from "../components/Jumbotron";
 import API from "../utils/API";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import { Col, Row, Container } from "../components/Grid";
-import { List, ListItem } from "../components/List";
+import React, { Component } from "react";
+// import  div  from "../components/ ";
 // import { Input, TextArea, FormBtn } from "../components/Form";
 
 class TestSelector extends Component {
@@ -15,21 +14,13 @@ class TestSelector extends Component {
     // synopsis: ""
   };
 
-  // componentDidMount() {
-  //   this.load();
-  // }
+  
 
   loadTests = () => {
     API.getTests()
       .then(res =>
         this.setState({ tests: res.data, subject: ""})
       )
-      .catch(err => console.log(err));
-  };
-
-  deleteTest = id => {
-    API.deleteTest(id)
-      .then(res => this.loadTests())
       .catch(err => console.log(err));
   };
 
@@ -55,36 +46,50 @@ class TestSelector extends Component {
 
   render() {
     return (
-      <Container fluid>
-        <Row>
-          <Col size="md-6">
-            <Jumbotron>
+    <div>
+      <div className="jumbotron">
               <h1>Available Tests</h1>
-            </Jumbotron>
-            
-          </Col>
-          <Col size="col-md">
-            <Jumbotron>
-              <h1>Tests to take</h1>
-            </Jumbotron>
-            {this.state.tests.length ? (
-              <List>
-                {this.state.tests.map(test => (
-                  <ListItem key={test._id}>
-                    <Link to={"/tests/" + test._id}>
-                      <strong>
-                        {test.subject}
-                      </strong>
-                    </Link>
-                  </ListItem>
-                ))}
-              </List>
-            ) : (
-              <h3>No Results to Display</h3>
-            )}
-          </Col>
-        </Row>
-      </Container>
+      </div>
+      <div className="container">
+        {/* <div className="row">
+          <div className="col" size="md">            */}
+                  <div className="card">
+                  <img className="card-img-top" src="..." alt="Card image cap"/>
+                  <div className="card-body">
+                    <h5 className="card-title">JavaScript</h5>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Get Certified</a>
+                  </div>
+
+                  <div className="card">
+                  <img className="card-img-top" src="..." alt="Card image cap"/>
+                  <div className="card-body">
+                    <h5 className="card-title">Python</h5>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Get Certified</a>
+                  </div>
+
+                  <div className="card">
+                  <img className="card-img-top" src="..." alt="Card image cap"/>
+                  <div className="card-body">
+                    <h5 className="card-title">React</h5>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Get Certified</a>
+                  </div>
+
+                  <div className="card">
+                  <img className="card-img-top" src="..." alt="Card image cap"/>
+                  <div className="card-body">
+                    <h5 className="card-title">CSS3</h5>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Get Certified</a>
+                  </div> 
+                  </div>
+                  </div>
+                </div>  
+                </div>                           
+          </div>
+    </div>
     );
   }
 }
