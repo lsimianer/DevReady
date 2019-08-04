c99b26359b5f4637eadb5e6d9f22f9185ad319ba
Mad Marine <pnnroman92@gmail.com>
2019-08-04 10:39:53 -0500

    created developer and recruiter schemas and created a route to populate users on the respective page

--- client/src/pages/CreateUser.js
+++ client/src/pages/CreateUser.js
@@ -3,7 +3,7 @@ import React from "react";
 function CreateUser() {
   return (
     <div>
-      <h1>ViewDevs page</h1>
+      <h1>Current Developer Applicants</h1>
       <p>
         this is where users will create an account
       </p>
