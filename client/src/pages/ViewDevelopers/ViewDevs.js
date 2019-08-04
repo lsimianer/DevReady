1542556916664d4a2eb72bac4b7e03661ebe7327
Mad Marine <pnnroman92@gmail.com>
2019-08-04 15:17:34 -0500

    put the current develops into List Item component for more styling

--- client/src/pages/ViewDevelopers/ViewDevs.js
+++ client/src/pages/ViewDevelopers/ViewDevs.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import API from "../../utils/API";
 import "./View.css";
+import { List, ListItem } from "../components/List";
 
 
 class ViewDevs extends Component  {
@@ -33,21 +34,23 @@ class ViewDevs extends Component  {
       <p>
         ReadyDev puts developers through extreme tests to ensure that they are ready to hit the ground running in any area they earn certifications in on our site.
       </p>
-
+      
       {this.state.developers.length ? (
-        this.state.developers.map(
-          dev =>(
-            <ul>
-              <li key={dev._id}>
+        <List>
+        {this.state.developers.map(
+          dev => (
+            
+              <ListItem key={dev._id}>
                     <a href={"/developers/" + dev._id}>
                       <strong>
                         {dev.name} by {dev.email}
                       </strong>
                     </a>
-                    </li>
-            </ul>
+                    </ListItem>
+                   
           )
-        )
+        )}
+        </List>
       ): (
         <h1>No Developers are Looking Right Now! Check Back Later!</h1>
       )} 
