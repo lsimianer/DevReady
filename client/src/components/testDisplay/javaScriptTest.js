5e8fb2f94076152a11946780b96b32658f8c5b8b
lukas simianer <lsimianer@gmail.com>
2019-08-05 22:34:13 -0500

    button error quick fix

--- client/src/components/testDisplay/javaScriptTest.js
+++ client/src/components/testDisplay/javaScriptTest.js
@@ -69,7 +69,7 @@ function javaScriptTest(props) {
           <input type="radio" value="4" />   {testData[4].a[3]}
         </div>
       </div>
-      <button
+      <button></button>
     </div>
   );
 }
