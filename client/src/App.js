9327cbceeb8f25222c48199043e696dfa56028cc
Charles Davidson <cddavidson86@yahoo.com>
2019-08-06 22:18:53 -0500

    added links to the Nav and the tests now score and populate.

--- client/src/App.js
+++ client/src/App.js
@@ -22,7 +22,6 @@ function App() {
     <Router>
       <div>
         <Nav />
-        {/* <div id="app"></div> */}
         <Switch>
           <Route exact path="/" component={Landing} />
           <Route exact path="/createDeveloper" component={CreateDeveloper} />
