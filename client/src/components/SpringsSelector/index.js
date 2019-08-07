e8fb32f33996d00a5c319864fc712ba2f543ac79
Charles Davidson <cddavidson86@yahoo.com>
2019-08-07 11:05:11 -0500

    resolving

--- client/src/components/SpringsSelector/index.js
+++ client/src/components/SpringsSelector/index.js
@@ -1,8 +1,7 @@
 // Original: https://tympanus.net/codrops/2017/03/15/stack-motion-hover-effects/
-import React, { useState } from 'react'
-import ReactDOM from 'react-dom'
-import { useSpring, useSprings, animated, interpolate } from 'react-spring'
-import './styles.css'
+import React, { useState } from 'react';
+import { useSpring, useSprings, animated, interpolate } from 'react-spring';
+import './style.css';
 
 function Stack({ image, background }) {
   const [open, setOpen] = useState(false)
@@ -25,22 +24,6 @@ function Stack({ image, background }) {
       ))}
     </div>
   )
-}
+};
 
-function App() {
-  return (
-    <div class="main">
-      <Stack image="https://uploads.codesandbox.io/uploads/user/b3e56831-8b98-4fee-b941-0e27f39883ab/9qWx-1.png" background="#52649e" />
-      <Stack image="https://uploads.codesandbox.io/uploads/user/b3e56831-8b98-4fee-b941-0e27f39883ab/T0hA-3.png" background="#f7f295" />
-      <Stack image="https://uploads.codesandbox.io/uploads/user/b3e56831-8b98-4fee-b941-0e27f39883ab/QoXU-2.png" background="#ee7074" />
-      <Stack image="https://uploads.codesandbox.io/uploads/user/b3e56831-8b98-4fee-b941-0e27f39883ab/QoXU-2.png" background="#ee7074" />
-      <Stack image="https://uploads.codesandbox.io/uploads/user/b3e56831-8b98-4fee-b941-0e27f39883ab/9qWx-1.png" background="#52649e" />
-      <Stack image="https://uploads.codesandbox.io/uploads/user/b3e56831-8b98-4fee-b941-0e27f39883ab/T0hA-3.png" background="#f7f295" />
-      <Stack image="https://uploads.codesandbox.io/uploads/user/b3e56831-8b98-4fee-b941-0e27f39883ab/T0hA-3.png" background="#f7f295" />
-      <Stack image="https://uploads.codesandbox.io/uploads/user/b3e56831-8b98-4fee-b941-0e27f39883ab/QoXU-2.png" background="#ee7074" />
-      <Stack image="https://uploads.codesandbox.io/uploads/user/b3e56831-8b98-4fee-b941-0e27f39883ab/9qWx-1.png" background="#52649e" />
-    </div>
-  )
-}
-
-ReactDOM.render(<App />, document.getElementById('root'))
+export default Stack;
