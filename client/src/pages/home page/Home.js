2efa33adb2eb4b1376a0fdc706e05e584c51ed94
lukas simianer <lsimianer@gmail.com>
2019-08-08 13:47:01 -0500

    home page done

--- client/src/pages/home page/Home.js
+++ client/src/pages/home page/Home.js
@@ -1,31 +1,40 @@
 import React from "react";
+import "../home page/home.css";
+import logo1 from "./logo1.png";
 
 function Home() {
   return (
     <div>
-      <div className="jumbotron-test">
+      <div className="jumbotron-home">
         <h1 id="header" >Welcome to ReadyDev</h1>
         <h2 id="subHeader" > Get Ready, Stay Ready</h2>
       </div>     
       <div className="container">
-      <div className="card">
-        <i class="devicon-javascript-plain"></i>
-          <div className="card-body">
-            <h5 className="card-title">Take Tests</h5>
-            <p className="card-text">Take tests to show employers, post on LinkedIn, and improve your skills </p>
-            <button  class="btn btn-primary"><a href="/testSelector">Get Ready</a></button>
+        <div className="row">
+          <div className="col-sm">
+           <div className="card" id="goldCard">
+            {/* <img id="logo" src={logo1} alt="logo"/> */}
+            <div className="card-body">
+              <h5 className="card-title">Take Tests</h5>
+              <p className="card-text">Take tests to show employers, post on LinkedIn, and improve your skills </p>
+              <button  class="btn btn-primary"><a href="/testSelector">Get Ready</a></button>
+            </div>
           </div>
-      </div>
-      <div className="card">
-        <i class="devicon-javascript-plain"></i>
-          <div className="card-body">
-            <h5 className="card-title">Take Courses </h5>
-            <p className="card-text">View our courses that prepare your for our tests and your career </p>
-            <button class="btn btn-primary">Coming Soon</button>
           </div>
-      </div>
+        
+        <div className="col-sm">
 
+          <div className="card" id="goldCard">
+          {/* <img id="logo" src={logo1} alt="logo"/> */}
+              <div className="card-body">
+                <h5 className="card-title">Take Courses </h5>
+                <p className="card-text">View our courses that prepare your for our tests and your career </p>
+                <button class="btn btn-primary">Coming Soon</button>
+              </div>
+          </div>
+        </div>
 
+        </div>
       </div>       
     </div>
   );
