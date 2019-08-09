e92499ccc456d0904ec1559a0913b2198cfb1f9b
Charles Davidson <cddavidson86@yahoo.com>
2019-08-09 13:37:49 -0500

    fixed some alignment

--- client/src/pages/CreateDeveloper/Contact/AboutUs.js
+++ client/src/pages/CreateDeveloper/Contact/AboutUs.js
@@ -14,13 +14,13 @@ function AboutUs() {
           <h4>WHO WE ARE</h4>
         </div>
       
-        <p className="card-text">
+        <p className="cardy-text">
         ReadyDev started as a solution to a genuine need. As the need for software engineers increases yearly so do the people running towards the career field.
         The mechanisms for entry into this field include the traditional cs degree, bootcamps, and self taught. None of the afforementioned prepare you for
         the code intensive tasks of the jobs new developers seek.</p>
 
         <h4> OUR SOLUTION</h4>
-        <p className="card-text">
+        <p className="cardy-text">
           We enable developers to test their skills, build confidence for interviews, and most of all we help companies hiring junior
           developers save time and money by screening applicants with a test they built or one of our skill level and language specific tests.
         </p>
