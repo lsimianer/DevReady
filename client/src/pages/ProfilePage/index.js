0d805cdedc5704ac91b439508dde030f9cba89e3
Mad Marine <pnnroman92@gmail.com>
2019-08-12 23:21:02 -0500

    changed profile page to a class component to add state so that it can start pulling data from the database

--- client/src/pages/ProfilePage/index.js
+++ client/src/pages/ProfilePage/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {Component} from "react";
 import { Link } from "react-router-dom";
 import "./ProfilePage.css";
 import jsLogo from "../../components/Images/jsImage.png";
@@ -6,18 +6,24 @@ import cssLogo from "../../components/Images/cssImage.png";
 import reactLogo from "../../components/Images/reactImage.png";
 import pythonLogo from "../../components/Images/pythonImage.png";
 
-function ProfilePage() {
+class ProfilePage extends Component {
+
+    state = {
+        developers : []
+    }
+
+    render(){
     return (
         <div className="container info-container">
             
             <div className="row">
                 <div className="col-12 info-card">
-                    <div class="card info-card">
-                        <div class="card-body top-card-text-inside">
-                            <h5 class="card-title name-display">Welcome Roman</h5>
-                            <p class="card-text">Current progress: %100</p>
-                            <p class="card-text">Email: </p>
-                            <p class="card-text">Company Name: </p>
+                    <div className="card info-card">
+                        <div className="card-body top-card-text-inside">
+                            <h5 className="card-title name-display">Welcome Roman</h5>
+                            <p className="card-text">Current progress: %100</p>
+                            <p className="card-text">Email: </p>
+                            <p className="card-text">Company Name: </p>
                         </div>
                     </div>
                 </div>
@@ -26,42 +32,42 @@ function ProfilePage() {
 
             <div className="row">
                 <div className="col-3">
-                    <div class="card card-text-inside">
-                        <img src={jsLogo} class="card-img-top card-logo-image" alt="..." />
-                        <div class="card-body card-text-color">
-                            <h5 class="card-title">JavaScript Test</h5>
-                            <p class="card-text"><h2>Your Score</h2></p>
-                            <h1><span class="badge badge-secondary">83</span></h1>
+                    <div className="card card-text-inside">
+                        <img src={jsLogo} className="card-img-top card-logo-image" alt="..." />
+                        <div className="card-body card-text-color">
+                            <h5 className="card-title">JavaScript Test</h5>
+                            <p className="card-text"><h2>Your Score</h2></p>
+                            <h1><span className="badge badge-secondary">83</span></h1>
                         </div>
                     </div>
                 </div>
                 <div className="col-3">
-                    <div class="card card-text-inside">
-                        <img src={cssLogo} class="card-img-top card-logo-image" alt="..." />
-                        <div class="card-body card-text-color">
-                            <h5 class="card-title">CSS Test</h5>
-                            <p class="card-text"><h2>Your Score</h2></p>
-                            <h1><span class="badge badge-secondary">82</span></h1>
+                    <div className="card card-text-inside">
+                        <img src={cssLogo} className="card-img-top card-logo-image" alt="..." />
+                        <div className="card-body card-text-color">
+                            <h5 className="card-title">CSS Test</h5>
+                            <p className="card-text"><h2>Your Score</h2></p>
+                            <h1><span className="badge badge-secondary">82</span></h1>
                         </div>
                     </div>
                 </div>
                 <div className="col-3">
-                    <div class="card card-text-inside">
-                        <img src={reactLogo} class="card-img-top card-logo-image" alt="..." />
-                        <div class="card-body card-text-color">
-                            <h5 class="card-title">CSS Test</h5>
-                            <p class="card-text"><h2>Your Score</h2></p>
-                            <h1><span class="badge badge-secondary">72</span></h1>
+                    <div className="card card-text-inside">
+                        <img src={reactLogo} className="card-img-top card-logo-image" alt="..." />
+                        <div className="card-body card-text-color">
+                            <h5 className="card-title">CSS Test</h5>
+                            <p className="card-text"><h2>Your Score</h2></p>
+                            <h1><span className="badge badge-secondary">72</span></h1>
                         </div>
                     </div>
                 </div>
                 <div className="col-3">
-                    <div class="card card-text-inside">
-                        <img src={pythonLogo} class="card-img-top card-logo-image" alt="..." />
-                        <div class="card-body card-text-color">
-                            <h5 class="card-title">Python Test</h5>
-                            <p class="card-text"><h2>Your Score</h2></p>
-                            <h1><span class="badge badge-secondary">52</span></h1>
+                    <div className="card card-text-inside">
+                        <img src={pythonLogo} className="card-img-top card-logo-image" alt="..." />
+                        <div className="card-body card-text-color">
+                            <h5 className="card-title">Python Test</h5>
+                            <p className="card-text"><h2>Your Score</h2></p>
+                            <h1><span className="badge badge-secondary">52</span></h1>
                         </div>
                     </div>
                 </div>
@@ -69,6 +75,6 @@ function ProfilePage() {
 
         </div>
     );
-}
+}}
 
 export default ProfilePage;
