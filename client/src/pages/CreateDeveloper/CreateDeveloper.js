88363a9306559d453660f72d1235d4d3d252fbc5
Amanda Lowell <amandalowell2016@gmail.com>
2019-08-13 10:39:35 -0500

    alignment

--- client/src/pages/CreateDeveloper/CreateDeveloper.js
+++ client/src/pages/CreateDeveloper/CreateDeveloper.js
@@ -93,40 +93,40 @@ class CreateDeveloper extends Component {
       
 
       <form>
-        <div className="form-group">
+        <div className="form-group box">
           <label htmlFor="username">Name</label>
           <br></br>
         <input value={this.state.developername}type="text/name" id="username"onChange={this.handleInputChange} className="form-control"placeholder="John Doe" name="developername"/> 
         </div>
         
-        <div className="form-group">
+        <div className="form-group box">
         <label htmlFor="emailaccount">Email</label>
         <br></br>
         <input value={this.state.email}onChange={this.handleInputChange} type="text/email" className="form-control" id="emailaccount" placeholder="Newdev@greatdev.com" name="email"/>
         </div>
-        <div className="form-group">
+        <div className="form-group box ">
         <label htmlFor="password">Password</label>
         <br></br>
         <input value={this.state.password}onChange={this.handleInputChange}type="text/password" className="form-control" id="password" placeholder="Password" name="password"/>
         <br></br>
         </div>
-        <p>How many years of experience do you have?</p>
+        <p class="yes">How many years of experience do you have?</p>
         <input type="number" value={this.state.experience} onChange={this.handleInputChange} name="experience"/>
         <br></br>
         <br></br> 
         
-        <p>Have you ever been convicted of a felony?</p>
+        <p class="yes">Have you ever been convicted of a felony?</p>
         <input type="radio" value={this.state.felony} onClick={this.felon} name="felony" value="true" checked={this.state.felony ? "checked" : ""} /> Yes 
         <input value="false" type="radio" value={this.state.felony} onClick={this.felonF} name="felony" checked={this.state.felony ? "" : "checked"} /> No 
         <br></br>
         <br></br>
-        <p>Are you a veteran?</p>
+        <p class="yes">Are you a veteran?</p>
         <input type="radio" value={this.state.veteran} value="true" onClick={this.vet} name="veteran" checked={this.state.veteran ? "checked" : ""}/> Yes <input type="radio" value={this.state.veteran} onClick={this.vetF} checked={this.state.veteran ? "" : "checked"} name="veteran" value="false"/> No 
         <br></br>
         <br></br>
         <br></br>
 
-        <div className="form-group">
+        <div className="form-group box">
           <label htmlFor="referredby">If referred by a Company who sent you?</label> 
           <select className="form-control" id="referredby" value={this.companyName} onChange={this.handleInputChange} name="companyname">
       <option>Apple</option>
@@ -138,7 +138,7 @@ class CreateDeveloper extends Component {
         </div>
         <br></br>
         
-        <div className="form-group">
+        <div className="form-group box">
         <label htmlFor="aboutme">Tell Us About Yourself!</label>
         <br></br>
         <textarea className="form-control" id="aboutme" rows="5" value={this.state.aboutMe} onChange={this.handleInputChange} name="aboutMe"></textarea>
